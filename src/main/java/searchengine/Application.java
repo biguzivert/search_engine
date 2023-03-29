020c76826abcfff305709b958ba40a8b916c339d
Джафар Гаибов <xdjafarx@gmail.com>
2023-03-29 15:04:02 +0300

    добавил логгирование в проект, настроил логгирование в application.yaml

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -1,11 +1,14 @@
 package searchengine;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.sql.DataSource;
 
+@Log4j2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
