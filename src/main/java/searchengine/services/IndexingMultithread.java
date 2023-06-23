a28a2b88cc931ecc8ef2c9776e261a064abebfe1
Джафар Гаибов <xdjafarx@gmail.com>
2023-06-23 17:48:45 +0300

    изменена логика индексирования (перенос части логики в IndexingServiceImpl.java), изменения многопоточной части IndexingMultithread.java

--- src/main/java/searchengine/services/IndexingMultithread.java
+++ src/main/java/searchengine/services/IndexingMultithread.java
@@ -6,7 +6,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import searchengine.model.Page;
 import searchengine.model.Site;
-import searchengine.model.enums.StatusEnum;
 import searchengine.services.repositories.PageRepository;
 import searchengine.services.repositories.SitesRepository;
 
@@ -16,70 +15,65 @@ import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class IndexingMultithread extends RecursiveTask<Site> {
-    private searchengine.config.Site site;
-
+    private Site site;
     private Page page;
     private SitesRepository sitesRepository;
     private PageRepository pageRepository;
 
-    private volatile String time;
+    private String link;
 
-    public IndexingMultithread(searchengine.config.Site site) {
+    private String statusTime;
+
+    public IndexingMultithread(Site site, String link) {
         this.site = site;
+        this.link = link;
     }
 
-    private IndexingMultithread(searchengine.config.Site site, Page page){
+ /*   private IndexingMultithread(Site site, Page page){
         this.site = site;
         this.page = page;
-    }
+    }*/
 
     @Override
     protected Site compute() {
         List<Site> subsites = new ArrayList<>();
         List<IndexingMultithread> tasks = new ArrayList<>();
-
-        //перенести в немногопоточную часть в IndexingImpl, чтобы реализовать передачу Page в IndexingMultithread а не Site
-        Site infoToDelete = sitesRepository.findSiteByUrl(site.getUrl());
-        sitesRepository.delete(infoToDelete);
-        pageRepository.deleteSite(infoToDelete);
-        Site updateTableSite = new Site();
-        updateTableSite.setStatus(StatusEnum.INDEXING);
-        updateTableSite.setName(site.getName());
-        updateTableSite.setUrl(site.getUrl());
-        sitesRepository.save(updateTableSite);
-
         try {
-            Document document = Jsoup.connect(site.getUrl()).userAgent("Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36").get();
+            Document document = Jsoup.connect(link).userAgent("Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36").get();
             Elements links = document.select("a");
-            if (!links.isEmpty()){
+            if (!links.isEmpty()) {
                 links.forEach(l -> {
                     Page page = new Page();
                     String path = l.attr("abs:href");
                     Element allPageCode = l.select("html").first();
                     String content = allPageCode.outerHtml();
-                    //получить корректный формат даты
-                    time = LocalDateTime.now().toString();
+                    statusTime = LocalDateTime.now().toString();
+
                     //получить код для Code Page
 
                     page.setPath(path);
-                    page.setSiteId(updateTableSite.getId());
+                    page.setSiteId(site.getId());
                     page.setContent(content);
+                    site.setStatusTime(statusTime);
 
-                    IndexingMultithread task = new IndexingMultithread(site, page);
+                    IndexingMultithread task = new IndexingMultithread(site, path);
                     task.fork();
                     tasks.add(task);
 
+
                 });
             }
-        }
-        catch (Exception exception) {
+            for (IndexingMultithread task : tasks) {
+                subsites.add(task.join());
+            }
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
 
-
-
+    //должен возвращать pages, соотвественно надо изменить тип данных метода
         return null;
     }
+}
 
 
     /*
@@ -111,7 +105,3 @@ public class IndexingMultithread extends RecursiveTask<Site> {
 */
 
 
-
-        return null;
-    }
-}
--- src/main/java/searchengine/services/IndexingServiceImpl.java
+++ src/main/java/searchengine/services/IndexingServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import searchengine.config.Site;
 import searchengine.config.SitesList;
 import searchengine.dto.statistics.IndexingResponse;
+import searchengine.model.Page;
 import searchengine.model.enums.StatusEnum;
 import searchengine.services.repositories.PageRepository;
 import searchengine.services.repositories.SitesRepository;
@@ -18,11 +19,22 @@ public class IndexingServiceImpl implements IndexingService{
 
     private IndexingMultithread indexingMultithread;
     private ForkJoinPool pool;
+    private SitesRepository sitesRepository;
+    private PageRepository pageRepository;
 
     public IndexingResponse startIndexing(){
         List<Site> sites = sitesList.getSites();
         for(Site site : sites){
-            pool.invoke(new IndexingMultithread(site));
+            searchengine.model.Site siteToDelete = sitesRepository.findSiteByUrl(site.getUrl());
+            sitesRepository.delete(siteToDelete);
+            pageRepository.deleteSite(siteToDelete);
+            searchengine.model.Site newSite = new searchengine.model.Site();
+            newSite.setName(site.getName());
+            newSite.setStatus(StatusEnum.INDEXING);
+            newSite.setUrl(site.getUrl());
+            sitesRepository.save(newSite);
+            String link = newSite.getUrl();
+            pool.invoke(new IndexingMultithread(newSite, link));
         }
 
         return null;
